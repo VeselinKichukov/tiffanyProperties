552e35ade46fcffe93b506cc105fcb83f87a293c
george_serbezov <george.serbezov@gmail.com>
2018-09-14 02:23:32 +0200

    Refactoring of style and files (Still in process)

--- resources/views/checkin.blade.php
+++ resources/views/checkin.blade.php
@@ -1,93 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Finance</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/reset.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/grid_12.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/slider-2.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/jqtransform.css">
-    <script src="js/jquery-1.7.min.js"></script>
-    <script src="js/jquery.easing.1.3.js"></script>
-    <script src="js/cufon-yui.js"></script>
-    <script src="js/vegur_400.font.js"></script>
-    <script src="js/Vegur_bold_700.font.js"></script>
-    <script src="js/cufon-replace.js"></script>
-    <script src="js/tms-0.4.x.js"></script>
-    <script src="js/jquery.jqtransform.js"></script>
-    <script src="js/FF-cash.js"></script>
-    <script>
-		$(document).ready(function(){
-			$('.form-1').jqTransform();
-			$('.slider')._TMS({
-				show:0,
-				pauseOnHover:true,
-				prevBu:'.prev',
-				nextBu:'.next',
-				playBu:false,
-				duration:1000,
-				preset:'fade',
-				pagination:true,
-				pagNums:false,
-				slideshow:7000,
-				numStatus:false,
-				banners:false,
-				waitBannerAnimation:false,
-				progressBar:false
-			})
-		});
-	</script>
-	<!--[if lt IE 8]>
-       <div style=' clear: both; text-align:center; position: relative;'>
-         <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
-           <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
-        </a>
-      </div>
-    <![endif]-->
-    <!--[if lt IE 9]>
-   		<script type="text/javascript" src="js/html5.js"></script>
-    	<link rel="stylesheet" type="text/css" media="screen" href="css/ie.css">
-	<![endif]-->
-</head>
-<body>
-<div class="main">
-<!--==============================header=================================-->
-<header>
-    <div>
-        <h1><a href="index.html"><img src="images/logo.jpg" alt=""></a></h1>
-        <div class="social-icons">
-            <span>Follow Us:</span>
+@extends('hello')
 
-            <a href="index_bg.html" class="language"></a>
-
-            <a href="#" class="icon-3"></a>
-            <a href="#" class="icon-2"></a>
-            <a href="#" class="icon-1"></a>
-        </div>
-        <div id="slide">
-            <div class="slider">
-                <ul class="items">
-                    <li><img src="images/slider_sinemorec_1.jpg" alt="" /></li>
-                    <li><img src="images/slider_sinemorec_2.jpg" alt="" /></li>
-                    <li><img src="images/slider_sinemorec_3.jpg" alt="" /></li>
-                </ul>
-            </div>
-            <a href="#" class="prev"></a><a href="#" class="next"></a>
-        </div>
-        <nav>
-            <ul class="menu">
-                <li><a href="index.html">Home</a></li>
-                <!-- <li><a href="buying.html">Buying</a></li> -->
-                <li><a href="appartments.html">Apartments</a></li>
-                <!-- <li><a href="renting.html">Renting</a></li> -->
-                <li class="current"><a href="checkin.html">Checkin Procedure</a></li>
-                <li><a href="contacts.html">Contacts</a></li>
-            </ul>
-        </nav>
-    </div>
-</header>
-<!--==============================content================================-->
+@section('content')
 <section id="content">
     <div class="container_12">
       <div class="grid_8">
@@ -149,14 +62,4 @@
       <div class="clear"></div>
     </div>
 </section>
-</div>
-<!--==============================footer=================================-->
-    <footer>
-        <p>© 2012 Real Estate</p>
-        <p>Website Template by <a href="http://www.templatemonster.com/" target="_blank" rel="nofollow">www.templatemonster</a></p>
-    </footer>
-<script>
-	Cufon.now();
-</script>
-</body>
-</html>
+@endsection
